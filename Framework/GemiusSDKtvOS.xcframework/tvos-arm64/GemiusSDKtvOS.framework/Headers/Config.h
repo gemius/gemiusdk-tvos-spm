5fb2b315f35c4294db4c6a0aa59398d08bdd7436
jlyskawka-n7 <85341371+jlyskawkaN7@users.noreply.github.com>
2025-07-23 12:14:03 +0200

    tvOS Version 2.2.0

--- Framework/GemiusSDKtvOS.xcframework/tvos-arm64/GemiusSDKtvOS.framework/Headers/Config.h
+++ Framework/GemiusSDKtvOS.xcframework/tvos-arm64/GemiusSDKtvOS.framework/Headers/Config.h
@@ -8,7 +8,13 @@
 #import <UIKit/UIKit.h>
 
 #define GEM_SDK_NAME    @"GemiusSDK"
-#define GEM_SDK_VERSION @"2.1.0"
+#define GEM_SDK_VERSION @"2.2.0"
+
+typedef NS_ENUM(NSUInteger, GEMDataProcessingConsentSource) {
+    GEMDataProcessingConsentSourceManual = 0,
+    GEMDataProcessingConsentSourceCMP,
+    GEMDataProcessingConsentSourceTCString,
+};
 
 NS_ASSUME_NONNULL_BEGIN
 
@@ -19,7 +25,7 @@ FOUNDATION_EXPORT NSNotificationName const GEMUserAgentDidChangeNotification;
 @property (nonatomic) BOOL loggingEnabled;
 @property (nonatomic) BOOL cookiesAllowed;
 @property (nonatomic) BOOL IDFACollectionEnabled;
-@property (nonatomic) BOOL useCmp;
+@property (nonatomic) GEMDataProcessingConsentSource dataProcessingConsentSource;
 @property (nonatomic) NSTimeInterval cmpTimeout;
 @property (nonatomic, copy) NSSet<NSNumber *> *audiencePurposes;
 @property (nonatomic, copy) NSSet<NSNumber *> *adOceanPurposes;
@@ -28,6 +34,7 @@ FOUNDATION_EXPORT NSNotificationName const GEMUserAgentDidChangeNotification;
 + (NSString *)getSdkVersion;
 
 - (void)setAppInfo:(NSString *)name version:(NSString *)version;
+- (void)setTCString:(nullable NSString *)tcString gdprApplies:(BOOL)gdprApplies;
 - (NSString *)getUA4WebView;
 - (void)restoreAudiencePurposes;
 - (void)restoreAdOceanPurposes;
